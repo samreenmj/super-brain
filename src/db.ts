58ba9552e0adf700605ae8555a0699e5c999500d
Samreen MJ <samreenmj@gmail.com>
2025-01-17 00:45:10 +0530

    ...

--- src/db.ts
+++ src/db.ts
@@ -1,2 +1,9 @@
+import mongoose, {model, Schema} from "mongoose";
+mongoose.connect("mongodb+srv://samreenmj:smrn7700@cluster0.tueqa.mongodb.net/")
 
+ const UserSchema = new Schema({
+    username:{type:String,unique:true},
+    password:String
+ })
 
+ export const UserModel = model("User",UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
+import { UserModel } from './db';
 
 
 const app = express();
+app.use (express.json());
 
 
 
 
-app.post("api/v1/signup",(req,res)=>{
+app.post("api/v1/signup",async(req,res)=>{
+    //zod validation ,hash password later
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await UserModel.create({
+        username:username,
+        password:password
+    })
+    res.json({
+        message:"user signed up"
+    })
 
 })
 
 app.post("api/v1/signin",(req,res)=>{
+    const username =req.body.username
+    const password = req.body.password
     
 })
 
@@ -35,3 +50,6 @@ app.post("api/v1/brain/share",(req,res)=>{
 app.get("api/v1/brain/:shareLink",(req,res)=>{
     
 })
+
+
+app.listen(3000);
